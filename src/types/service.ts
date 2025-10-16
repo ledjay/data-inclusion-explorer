905f724508175265d1b30c4e35e17ea5ffba83ae
Jérémie Gisserot <j@jeremie-gisserot.net>
2025-10-16 15:51:12 +0200

    :sparkles: Fiche view

--- src/types/service.ts
+++ src/types/service.ts
@@ -10,13 +10,27 @@ export type Service = {
   type?: string;
   thematiques?: string[];
   frais?: string;
+  frais_precisions?: string;
   publics?: string[];
+  publics_precisions?: string;
   commune?: string;
   code_postal?: string;
+  code_insee?: string;
+  adresse?: string;
+  complement_adresse?: string;
+  longitude?: number;
+  latitude?: number;
   telephone?: string;
   courriel?: string;
   modes_accueil?: string[];
+  zone_eligibilite?: string[];
+  conditions_acces?: string;
   lien_mobilisation?: string;
+  modes_mobilisation?: string[];
+  mobilisable_par?: string[];
+  mobilisation_precisions?: string;
+  horaires_accueil?: string;
+  score_qualite?: number;
 };
 
 export type ServiceSearchResult = {
